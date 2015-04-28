73df5b03810a085a19db9594987f24cd123fbc50
ronaldvanduren <ronaldvanduren@hotmail.com>
2015-04-28 22:59:52 +0200

    Deleted repo content

--- app/js/offcanvas.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Ronald on 25-4-2015.
- */
-$(document).ready(function () {
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('.row-offcanvas').toggleClass('active')
-    });
-});
